950a953e5930e53e5751e57c04d7e268b673215f
Sean Brindley <seanbrindley17@gmail.com>
2024-12-09 16:43:22 +0000

    Create Category and Product models. Import Category and Product models to admin.py. Migrate to Django database to allow for creation of data within database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_display_name(self):
+        return self.display_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        "Category", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField
+    is_club_branded = models.BooleanField(default=False)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    is_gendered = models.BooleanField(default=False, null=True, blank=True)
+    is_equipment = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
