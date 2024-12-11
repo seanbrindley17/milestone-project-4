c51ed19ee4d0bdb79a4020aa6cc11530354aa000
Sean Brindley <seanbrindley17@gmail.com>
2024-12-11 16:10:29 +0000

    Add product_display to be included in index.html. Add barebones basic view to display products within homepage

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .models import Category, Product
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "home/index.html")
+    products = Product.objects.all()
+
+    context = {"products": products}
+
+    return render(request, "home/index.html", context)
