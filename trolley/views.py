44523d322f7779259d81df141e414849b6970dba
Sean Brindley <seanbrindley17@gmail.com>
2024-12-17 15:01:37 +0000

    Create Trolley app, initial display view and working url and connect to relevant link to direct user to the trolley page

--- trolley/views.py
+++ trolley/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def show_trolley(request):
+
+    return render(request, "trolley/trolley.html")
--- wnsc_shop/urls.py
+++ wnsc_shop/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("", include("home.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("trolley/", include("trolley.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
