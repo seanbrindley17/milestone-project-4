731dfbfc65ba73bf4ca4e331b039d52886fcca23
Sean Brindley <seanbrindley17@gmail.com>
2025-01-08 18:18:21 +0000

    Add code to allow the item and quantity of item selected to appear in the user's trolley. Needs refining but it's a start

--- trolley/views.py
+++ trolley/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,38 @@ from django.shortcuts import render
 def show_trolley(request):
 
     return render(request, "trolley/trolley.html")
+
+
+# View for a user to add a product to their trolley, tracked using the item_id
+def add_to_trolley(request, item_id):
+
+    return_url = request.POST.get("return-url")
+    quantity = int(request.POST.get("product-quantity"))
+
+    size = None
+    if "product_size" in request.POST:
+        size = request.POST["product-size"]
+
+    shoesize = None
+    if "shoe_size" in request.POST:
+        shoesize = request.POST["shoe-size"]
+
+    personalisation = None
+    if "product_personalisation_input" in request.POST:
+        personalisation = request.POST["product-personalisation-input"]
+
+    # Access session to check if trolley variable exists, if not initialise empty dictionary
+    trolley = request.session.get("trolley", {})
+
+    # If there's already an item_id key matching a key in the trolley dictionary
+    if item_id in list(trolley.keys()):
+        # Then the quantity is incremented accordingly
+        trolley[item_id] += quantity
+    else:
+        # Otherwise set the item_id key to match the quantity of the product added
+        trolley[item_id] = quantity
+
+    # Overwrite the previous Trolley with updated trolley session
+    request.session["trolley"] = trolley
+    print(request.session["trolley"])
+    return redirect(return_url)
