690012534ddea8b370d927acb5e370323344f59b
Sean Brindley <seanbrindley17@gmail.com>
2025-01-14 14:30:11 +0000

    Add signals.py to checkout app for dynamic cost updating

---
+++ checkout/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import OrderItem
+
+
+# Receiving signals from OrderItem model to update post_save
+@receiver(post_save, sender=OrderItem)
+# Will update order cost when item is updated or created
+def update_on_save(sender, instance, created, **kwargs):
+    # Update total of instance of order referred to
+    instance.order.update_total()
+
+
+# Updates order cost upon item delete
+@receiver(post_delete, sender=OrderItem)
+def update_on_delete(sender, instance, **kwargs):
+
+    instance.order.update_total()
