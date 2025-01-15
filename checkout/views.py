9e44567375e4925b092e3d9c42af93b88da24312
Sean Brindley <seanbrindley17@gmail.com>
2025-01-15 19:31:45 +0000

    Add caviat to view_checkout to prevent users brute forcing their way into it. Include orderform and context to allow checkout form to be accessed properly

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 # Create your views here.
 
 
+# View for the checkout
 def view_checkout(request):
 
-    return render(request, "checkout/checkout.html")
+    trolley = request.session.get("trolley", {})
+    # If nothing is in the trolley dictionary
+    if not trolley:
+        # Display error message
+        messages.error(request, "No items in trolley")
+        # Return user to home page to prevent access by inputting the checkout url
+        return redirect(reverse("home"))
+
+    order_form = OrderForm()
+    context = {order_form: order_form}
+
+    return render(request, context, "checkout/checkout.html")
