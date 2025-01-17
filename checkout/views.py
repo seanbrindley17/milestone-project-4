f15d5829b96827d388f8fab87cd22a19895d039b
Sean Brindley <seanbrindley17@gmail.com>
2025-01-17 15:24:51 +0000

    Add trolley items context to view along with stripe api key

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,22 @@
+import stripe
+from decimal import Decimal
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+from trolley.contexts import trolley_items
 from .forms import OrderForm
+
 
 # Create your views here.
 
 
 # View for the checkout
 def view_checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    stripe.api_key = stripe_secret_key
 
     trolley = request.session.get("trolley", {})
     # If nothing is in the trolley dictionary
@@ -16,10 +26,15 @@ def view_checkout(request):
         # Return user to home page to prevent access by inputting the checkout url
         return redirect(reverse("home"))
 
+    trolley_items_context = trolley_items(request)
+    order_cost = trolley_items_context["order_cost"]
+    delivery_charge = trolley_items_context["delivery_charge"]
+    total_cost = trolley_items_context["total_cost"]
+
     order_form = OrderForm()
     context = {
         "order_form": order_form,
-        "stripe_public_key": "pk_test_51QhxeeJIWLpCU8q8Mf6QMfrNuhh8EE0b0PVHDxLE4FRhFpECs9p8DiI2Lkjftaty1jWXHcYx0ZMJRfPA6aucMgJs00fjUMHKTH",
+        "stripe_public_key": stripe_public_key,
         "client_secret": "test client secret",
     }
     template = "checkout/checkout.html"
