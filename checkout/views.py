0a7beab84e1e9e2cb59e1a2519d5147e8fa2efa1
Sean Brindley <seanbrindley17@gmail.com>
2025-01-17 18:18:43 +0000

    Add logic to deal with when the checkout form is posted, with the form data being added, and the items in the trolley iterated through and saved in the order form

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,11 @@
 import stripe
 from decimal import Decimal
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+from home.models import Product
 from trolley.contexts import trolley_items
+from .models import Order, OrderItem
 from .forms import OrderForm
 
 
@@ -16,7 +18,75 @@ def view_checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    stripe.api_key = stripe_secret_key
+    # If form is being posted
+    if request.method == "POST":
+        trolley = request.session.get("trolley", {})
+
+        # Save form data in variable form_data
+        form_data = {
+            "name": request.POST["name"],
+            "surname": request.POST["surname"],
+            "email": request.POST["email"],
+            "phone_number": request.POST["phone_number"],
+            "town_or_city": request.POST["town_or_city"],
+            "address_line_one": request.POST["address_line_one"],
+            "address_line_two": request.POST["address_line_two"],
+            "county": request.POST["county"],
+            "postcode": request.POST["postcode"],
+        }
+        # Passes form_data above to the OrderForm in forms.py and save as order_form
+        order_form = OrderForm(form_data)
+        # Will save the order form if it's valid
+        if order_form.is_valid():
+            order = order_form.save()
+            # Loop through items in trolley
+            for item_id, item_data in trolley.items():
+                try:
+                    # Get the item id to determine whether it has sizes, shoesizes or not
+                    product = Product.objects.get(id=item_id)
+                    # if item_data is an integer, it doesn't have a size
+                    if isinstance(item_data, int):
+                        order_item = OrderItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_item.save()
+                    elif isinstance(item_data["items_by_size"]):
+                        for size, quantity in item_data["items_by_size"].items():
+                            order_item = OrderItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_item.save()
+                    else:
+                        for shoesize, quantity in item_data[
+                            "items_by_shoesize"
+                        ].items():
+                            order_item = OrderItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=shoesize,
+                            )
+                except Product.DoesNotExist:
+                    messages.error(
+                        request,
+                        ("A product in the trolley was not found in the the database"),
+                    )
+                    order.delete()
+                    return redirect(reverse("show_trolley"))
+
+            request.session["save_info"] = "save-info" in request.POST
+            return redirect(reverse("checkout_success", args=[order.order_number]))
+        else:
+            messages.error(
+                request,
+                "There was an error with your form. \
+                Please double check your information.",
+            )
 
     trolley = request.session.get("trolley", {})
     # If nothing is in the trolley dictionary
@@ -31,6 +101,9 @@ def view_checkout(request):
     order_cost = trolley_items_context["order_cost"]
     delivery_charge = trolley_items_context["delivery_charge"]
     total_cost = trolley_items_context["total_cost"]
+
+    # sets api key to secret key
+    stripe.api_key = stripe_secret_key
 
     # Payment intent
     total_cost_money = int(total_cost * 100)
