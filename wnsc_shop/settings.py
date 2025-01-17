8d1c78f3ad043c2a6a70e6056ec64ce3eb93d046
Sean Brindley <seanbrindley17@gmail.com>
2025-01-17 15:42:37 +0000

    Add event listener to stripe element javascript

--- wnsc_shop/settings.py
+++ wnsc_shop/settings.py
@@ -161,6 +161,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Delivery Percentage
 
 DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = "gbp"
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
