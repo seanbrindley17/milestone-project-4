69971039f15b24b3d8d71eac9edb1a185095c231
Sean Brindley <seanbrindley17@gmail.com>
2025-01-20 19:18:36 +0000

    Add working code to handle form submission and error messages in the card payment successfully

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,24 +1,21 @@
-const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-const clientSecret = $('#id_client_secret').text().slice(1, -1);
-const stripe = stripe(stripePublicKey);
+const stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+const clientSecret = $("#id_client_secret").text().slice(1, -1);
+const stripe = Stripe(stripePublicKey);
+const elements = stripe.elements();
+const style = {
+};
+const card = elements.create("card", {style: style});
+card.mount('#card-element');
 
-const appearance = { /* appearance */ };
-const options = { layout: 'accordion', /* options */ };
-const elements = stripe.elements({ clientSecret, appearance });
-const paymentElement = elements.create('payment', options);
-paymentElement.mount('#payment-element');
-
-//Handle errors
-card.addEventListener("change", function(event) {
-    // Error detected
+// Handle realtime validation errors on the card element
+card.addEventListener("change", function (event) {
     const errorDiv = document.getElementById("card-errors");
-    // Show alert
     if (event.error) {
         const html = `
-        <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
-        </span>
-        <span>${event.error.message}</span>
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
         `;
         $(errorDiv).html(html);
     } else {
@@ -27,33 +24,34 @@ card.addEventListener("change", function(event) {
 });
 
 // Handle form submit
+const form = document.getElementById("payment-form");
 
-const form = document.getElementById('payment-form');
-
-form.addEventListener('submit', function(ev) {
+form.addEventListener("submit", function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
+    card.update({ "disabled": true});
+    $("#submit-button").attr("disabled", true);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
+            billing_details: {
+                address: {
+                    country: "GB"
+                }
+            }
         }
     }).then(function(result) {
         if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            // Shows error message in html
-            var html = `
+            const errorDiv = document.getElementById("card-errors");
+            const html = `
                 <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
-            // re enables card update and submit button for user to fix issue
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
+            card.update({ "disabled": false});
+            $("#submit-button").attr("disabled", false);
         } else {
-            // If the status of the payment attempt is succeeded, then submit form
-            if (result.paymentIntent.status === 'succeeded') {
+            if (result.paymentIntent.status === "succeeded") {
                 form.submit();
             }
         }
