da5bbb2f889e5f9670eac7d5a6333ec4b95d352d
Sean Brindley <seanbrindley17@gmail.com>
2025-01-21 16:03:53 +0000

    Add post for cache_checkout_data and move stripe js within the done function of post to only execute if succesful response if received

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -30,50 +30,69 @@ form.addEventListener("submit", function(ev) {
     ev.preventDefault();
     card.update({ "disabled": true});
     $("#submit-button").attr("disabled", true);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            // billing details and shipping from form data to be used with webhooks
-            billing_details: {
-                name: $.trim(form.name.value),
+    // If user has saved info checked, retrieve from id-save-info 
+    const saveInfo = Boolean($("#id-save-info").attr("checked"));
+    // Get csrf token from form
+    const csrfToken = $("input[name='csrfmiddlewaretoken']").val();
+    // collate data to be sent to form in postData object
+    const postData = {
+        "csrfmiddlewaretoken": csrfToken,
+        "client_secret": clientSecret,
+        "save_info": saveInfo,
+    };
+    const url = "/checkout/cache_checkout_data";
+    // Post data to url above using postData above
+    // Uses .done to call function that will be executed if view has success response
+    // Basically means execute stripe function if all goes to plan
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                // billing details and shipping from form data to be used with webhooks
+                billing_details: {
+                    name: $.trim(form.name.value),
+                    surname: $.trim(form.surname.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line_one: $.trim(form.address_line_one.value),
+                        line_two: $.trim(form.address_line_two.value),
+                        town: $.trim(form.town_or_city.value),
+                        country: "GB",
+                    }
+                }
+            },
+            shipping: {
+               name: $.trim(form.name.value),
                 surname: $.trim(form.surname.value),
                 phone: $.trim(form.phone_number.value),
-                email: $.trim(form.email.value),
                 address: {
                     line_one: $.trim(form.address_line_one.value),
                     line_two: $.trim(form.address_line_two.value),
                     town: $.trim(form.town_or_city.value),
-                    country: "GB",
+                    postcode: $.trim(form.postcode.value),
+                    country: "GB", 
                 }
             }
-        },
-        shipping: {
-           name: $.trim(form.name.value),
-            surname: $.trim(form.surname.value),
-            phone: $.trim(form.phone_number.value),
-            address: {
-                line_one: $.trim(form.address_line_one.value),
-                line_two: $.trim(form.address_line_two.value),
-                town: $.trim(form.town_or_city.value),
-                postcode: $.trim(form.postcode.value),
-                country: "GB", 
+        }).then(function(result) {
+            if (result.error) {
+                const errorDiv = document.getElementById("card-errors");
+                const html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                card.update({ "disabled": false});
+                $("#submit-button").attr("disabled", false);
+            } else {
+                if (result.paymentIntent.status === "succeeded") {
+                    form.submit();
+                }
             }
-        }
-    }).then(function(result) {
-        if (result.error) {
-            const errorDiv = document.getElementById("card-errors");
-            const html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            card.update({ "disabled": false});
-            $("#submit-button").attr("disabled", false);
-        } else {
-            if (result.paymentIntent.status === "succeeded") {
-                form.submit();
-            }
-        }
-    });
+        });
+    // If view returns 400 response, reload page and django will show error message
+    }).fail(function() {
+        location.reload();
+    });  
 });
