d17e197b8b35e20e50d996db791b2ca3d3b435ea
Sean Brindley <seanbrindley17@gmail.com>
2025-01-22 17:09:35 +0000

    Add profile app to settings. Create initial UserProfile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# View for a user's profile and default information
+class UserProfile(models.Model):
+    # OneToOneField specifies that user can only have one profile
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    postcode = models.CharField(max_length=10, null=True, blank=True)
+    town_or_city = models.CharField(max_length=30, null=True, blank=True)
+    address_line_one = models.CharField(max_length=50, null=True, blank=True)
+    address_line_two = models.CharField(max_length=50, null=True, blank=True)
+    county = models.CharField(max_length=30, null=True, blank=True)
--- wnsc_shop/settings.py
+++ wnsc_shop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "trolley",
     "checkout",
     "contact",
+    "profiles",
 ]
 
 MIDDLEWARE = [
