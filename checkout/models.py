32167fd8b8bbaaf1bce7c045d9f786261c9e6629
Sean Brindley <seanbrindley17@gmail.com>
2025-01-22 17:23:02 +0000

    Add user_profile to Order model. Fix receiver signal User argument needing a capital U. Migrate models

--- checkout/models.py
+++ checkout/models.py
@@ -3,11 +3,19 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from home.models import Product
+from profiles.models import UserProfile
 
 
 # Create your models here.
 # Model for collating a user's order
 class Order(models.Model):
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders",
+    )
     order_number = models.CharField(max_length=32, null=False, editable=False)
     name = models.CharField(max_length=20, null=False, blank=False)
     surname = models.CharField(max_length=35, null=False, blank=False)
--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
@@ -22,7 +22,7 @@ class UserProfile(models.Model):
 
 
 # Signal for post_save event so that profile is either created or updated when user object is created
-@receiver(post_save, sender=user)
+@receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
