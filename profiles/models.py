1dcda632f166c79c8758fe506a4063779528bc16
Sean Brindley <seanbrindley17@gmail.com>
2025-01-22 17:15:33 +0000

    Add string method to return userprofile name to itself. Add signal for post save event for profile creation or updating

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -14,3 +16,14 @@ class UserProfile(models.Model):
     address_line_one = models.CharField(max_length=50, null=True, blank=True)
     address_line_two = models.CharField(max_length=50, null=True, blank=True)
     county = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+# Signal for post_save event so that profile is either created or updated when user object is created
+@receiver(post_save, sender=user)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
