b1b399f206ddf13e4eda4e7ff93b0e938922b92b
Sean Brindley <seanbrindley17@gmail.com>
2025-01-22 17:29:00 +0000

    Add basic profile view, url to urls.py and include that url in project level urls.py

--- wnsc_shop/urls.py
+++ wnsc_shop/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path("trolley/", include("trolley.urls")),
     path("checkout/", include("checkout.urls")),
     path("contact/", include("contact.urls")),
+    path("profile/", include("profile.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
