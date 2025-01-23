9177e0e855d6b36e7abf69a5db8737010aa1bc48
Sean Brindley <seanbrindley17@gmail.com>
2025-01-23 16:07:57 +0000

    Add Post logic for user to update information

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -7,13 +8,19 @@ from .forms import UserProfileForm
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    form = UserProfileForm(isinstance=profile)
+
+    # if statement for handling form post requests
+    if request.method == "POST":
+        # Create new instance of form using the posted data, updating profile got above
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully")
+
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = "profiles/profile.html"
-    context = {
-        "form": form,
-        "orders": orders,
-    }
+    context = {"form": form, "orders": orders, "on_profile_page": True}
 
     return render(request, template, context)
