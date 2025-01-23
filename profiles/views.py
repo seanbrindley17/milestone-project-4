7711bec0fa2f1939cd3f00eccd6dcfa7bccaa6cc
Sean Brindley <seanbrindley17@gmail.com>
2025-01-23 15:32:09 +0000

    Add form and orders to profile form to be rendered in template

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(isinstance=profile)
+    orders = profile.orders.all()
+
     template = "profiles/profile.html"
     context = {
-        "profile": profile,
+        "form": form,
+        "orders": orders,
     }
 
     return render(request, template, context)
