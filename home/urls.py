605526d1de0b42464e164ab1b4e7a3701a71504b
Sean Brindley <seanbrindley17@gmail.com>
2025-01-24 16:26:13 +0000

    Add edit product url

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("<int:product_id>/", views.product_detail, name="product_detail"),
     path("add_product/", views.add_product, name="add_product"),
+    path("edit/<int>product_id", views.edit_product, name="edit_product"),
 ]
