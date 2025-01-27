f4e68bab48ed2f590cfdc0d93669497cc9c76ee0
Sean Brindley <seanbrindley17@gmail.com>
2025-01-27 18:06:45 +0000

    Remove trolley from show trolley path

--- trolley/urls.py
+++ trolley/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.show_trolley, name="show_trolley"),
     path("add_product/<item_id>", views.add_to_trolley, name="add_to_trolley"),
-    path("remove/<item_id>", views.remove_item, name="remove_item")
+    path("remove/<item_id>", views.remove_item, name="remove_item"),
 ]
