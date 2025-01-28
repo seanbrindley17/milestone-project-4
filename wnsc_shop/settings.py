8ac1b72e9324a9f4b256dd9a15efdd0980576094
Sean Brindley <seanbrindley17@gmail.com>
2025-01-28 18:03:04 +0000

    Add AWS things to settings. Create custom_storages

--- wnsc_shop/settings.py
+++ wnsc_shop/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     "checkout",
     "contact",
     "profiles",
+    # Other
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -169,6 +171,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if "USE_AWS" in os.environ:
+    # Bucket config
+    AWS_STORAGE_BUCKET_NAME = "milestone-project-4-sb"
+    AWS_S3_REGION_NAME = "eu-north-1"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    # Static and media storage
+    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STATICFILES_LOCATION = "static"
+    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
+    MEDIAFILES_LOCATION = "media"
+
+    # Overwrite static and media urls in production
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 # Stripe
 
