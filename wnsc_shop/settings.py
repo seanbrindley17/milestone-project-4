b66dfc84b9c53d2751da3ac51f32da339f4f6f8b
Sean Brindley <seanbrindley17@gmail.com>
2025-01-29 13:52:41 +0000

    Replace staticfiles_storage and default_file_storage with up to date django setting

--- wnsc_shop/settings.py
+++ wnsc_shop/settings.py
@@ -190,9 +190,13 @@ if "USE_AWS" in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
     # Static and media storage
-    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STORAGES = {
+        "default": {
+            "BACKEND": "custom_storages.MediaStorage",
+        },
+        "staticfiles": {"BACKEND": "custom_storages.StaticStorage"},
+    }
     STATICFILES_LOCATION = "static/"
-    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
     MEDIAFILES_LOCATION = "media/"
 
     # Overwrite static and media urls in production
