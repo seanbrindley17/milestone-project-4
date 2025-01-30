ebfa506a9a63473493913495c2673c316253f7c6
Sean Brindley <seanbrindley17@gmail.com>
2025-01-30 18:11:36 +0000

    Environment variable stuff

--- wnsc_shop/settings.py
+++ wnsc_shop/settings.py
@@ -16,6 +16,12 @@ import os
 import dj_database_url
 
 
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback_secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [
     "milestone-project-4-sb-1448d349aa27.herokuapp.com",
