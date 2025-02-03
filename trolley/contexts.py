e14db78a0644bf1724b75ce0aee6815d77d31022
Sean Brindley <seanbrindley17@gmail.com>
2025-02-03 15:17:28 +0000

    Improve context so that size now is passed correctly and can be used in templates

--- trolley/contexts.py
+++ trolley/contexts.py
@@ -16,22 +16,45 @@ def trolley_items(request):
     trolley = request.session.get("trolley", {})
 
     # Iterates through the products in the trolley dictionary using the .items() method. This was confusing me intially
-    for item_id, quantity in trolley.items():
-        product = get_object_or_404(Product, pk=item_id)
-        # increments the product_count by the quantity added to the bag
-        product_count += quantity
-        # Calculates the total cost of the added item, multiply the quantity by the price. Simple stuff really
-        order_cost += quantity * product.price
-        if order_cost > 0:
-            delivery_charge = order_cost * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-            total_cost = order_cost + delivery_charge
-
-        items_in_trolley.append(
-            {
-                "product": product,
-                "quantity": quantity,
-            }
-        )
+    for item_id, item_data in trolley.items():
+        # If item_data is just an integer then there's no sizings involved
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            # increments the product_count by the quantity added to the bag
+            product_count += item_data
+            # Calculates the total cost of the added item, multiply the quantity by the price. Simple stuff really
+            order_cost += item_data * product.price
+            if order_cost > 0:
+                delivery_charge = order_cost * Decimal(
+                    settings.DELIVERY_PERCENTAGE / 100
+                )
+                total_cost = order_cost + delivery_charge
+                items_in_trolley.append(
+                    {
+                        "item_id": item_id,
+                        "product": product,
+                        "quantity": item_data,
+                    }
+                )
+        else:
+            # iterates through items that have a traditional size
+            product = get_object_or_404(Product, pk=item_id)
+            # Uses items_by_size to iterate the price and quantity accordingly
+            for size, quantity in item_data["items_by_size"].items():
+                order_cost += quantity * product.price
+                if order_cost > 0:
+                    delivery_charge = order_cost * Decimal(
+                        settings.DELIVERY_PERCENTAGE / 100
+                    )
+                    total_cost = order_cost + delivery_charge
+                    items_in_trolley.append(
+                        {
+                            "item_id": item_id,
+                            "quantity": quantity,
+                            "product": product,
+                            "size": size,
+                        }
+                    )
 
     context = {
         "items_in_trolley": items_in_trolley,
